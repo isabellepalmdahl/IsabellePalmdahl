b8eb6472e69fc2b9ba836531f3910814edd2ae7f
IsabellePalmdahl <isabellepalmdahl@hotmail.com>
2023-06-05 18:42:23 +0100

    ServicesController & Service views created Smaller changes in Layout

---
+++ IsabellePalmdahl/Controllers/ServicesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IsabellePalmdahl.Controllers
+{
+	public class ServicesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult WebDevelopment() {
+			return View(); 
+		}
+
+		public IActionResult AppDevelopment()
+		{
+			return View();
+		}
+
+		public IActionResult GraphicDesign()
+		{
+			return View();
+		}
+
+		public IActionResult Marketing()
+		{
+			return View();
+		}
+
+		public IActionResult LeadGeneration()
+		{
+			return View();
+		}
+	}
+}
